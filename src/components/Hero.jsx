bc054239071d74bd1cf14316875242a4673916f5
Illia Movchko <illia.movchko.concept@gmail.com>
2024-07-13 20:20:20 +0000

    Create Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section className="flex justify-center items-center">
+      <h1 className=""></h1>
+      <img className="" src="" alt="Hero image"></img>
+    </section>
+  );
+}
